fdcc9eb8f49284241f78ab2e8d205760d7565965
liiyq <174742815+liiyq@users.noreply.github.com>
2026-05-21 00:38:30 +0800

    feat:save reviews to DuckDB

--- src/ingestion.py
+++ src/ingestion.py
@@ -14,5 +14,12 @@ def load_reviews(max_samples: int):
     print(df.head())
     return df
 
+def save_to_duckdb(df: pd.DataFrame, db_path: str):
+    logger.info(f"Saving to DuckDB at {db_path}...")
+    conn = duckdb.connect(db_path)
+    conn.execute("CREATE TABLE IF NOT EXISTS reviews AS SELECT * FROM df")
+    conn.close()
+
 if __name__ == "__main__":
-    load_reviews(max_samples=1000)
+    df = load_reviews(max_samples=1000)
+    save_to_duckdb(df, db_path='data/reviews.duckdb')
